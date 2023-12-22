380a3679fd3a04d2c36ae5652ab9319daba2595c
ShadabAkh <shadab.akhter@hybridskill.com>
2023-12-22 14:49:55 +0530

    earning

---
+++ src/Components/Earnings/Doctordetails.jsx
@@ -0,0 +1,43 @@
+import doctorimage from '../Assets/image-slEubuAxR-transformed.png';
+import doctorsimage from '../Assets/image-RMOC43HSxZ-transformed.png';
+import doctoresimage from '../Assets/image-Ow_nE8fdA-transformed.png';
+const Doctordetails = [
+    {
+        id:1,
+        name:"Courtney Henry",
+        description:"physiotherapist",
+        image:doctorimage,
+    },
+    {
+        id:2,
+        name:"Courtney Henry",
+        description:"physiotherapist",
+        image:doctorimage,
+    },
+    {
+        id:3,
+        name:"Devon Lane",
+        description:"physiotherapist",
+        image:doctorsimage,
+    },
+    {
+        id:4,
+        name:"Devon Lane",
+        description:"physiotherapist",
+        image:doctorsimage,
+    },
+    {
+        id:5,
+        name:"Floyd Miles",
+        description:"physiotherapist",
+        image:doctoresimage,
+    },
+    {
+        id:6,
+        name:"Floyd Miles",
+        description:"physiotherapist",
+        image:doctoresimage,
+    }
+
+]
+export default Doctordetails
